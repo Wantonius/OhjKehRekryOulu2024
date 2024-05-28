eb7097ae49221174c9d207fe1560cb14615e8e20
Erno Hentonen <erno.hentonen@opiframe.com>
2024-05-28 13:59:27 +0300

    react 2

---
+++ react/05_lists/src/App.tsx
@@ -0,0 +1,44 @@
+import {useState} from 'react';
+import Contact from './models/Contact';
+import ContactForm from './components/ContactForm';
+
+interface State {
+	list:Contact[];
+	id:number;
+}
+
+function App() {
+ 
+	const [state,setState] = useState<State>({
+		list:[],
+		id:100
+	})
+
+	const addContact = (contact:Contact) => {
+		setState((state) => {
+			contact.id = state.id;
+			return {
+				list:state.list.concat(contact),
+				id:state.id+1
+			}
+		})
+	}
+
+	const removeContact = (id:number) => {
+		setState((state) => {
+			let tempList = state.list.filter(contact => contact.id !== id)
+			return {
+				...state,
+				list:tempList
+			}
+		})
+	}
+
+	return (
+		<>
+			<ContactForm addContact={addContact}/>
+		</>
+	)
+}
+
+export default App
---
+++ react/05_lists/src/models/Contact.tsx
@@ -0,0 +1,4 @@
+export default class Contact {
+
+	constructor(public firstname:string,public lastname:string,public email:string,public phone:string,public id:number) {}
+}
