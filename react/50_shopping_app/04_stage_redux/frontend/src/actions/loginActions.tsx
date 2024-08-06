cf33936c21146864f69568634e16fd50ce283e47
Erno Hentonen <erno.hentonen@opiframe.com>
2024-08-06 13:00:26 +0300

    action creators

--- react/50_shopping_app/04_stage_redux/frontend/src/actions/loginActions.tsx
+++ react/50_shopping_app/04_stage_redux/frontend/src/actions/loginActions.tsx
@@ -8,7 +8,48 @@ interface Token {
 }
 
 //ASYNC THUNKS
+export const register = (user:User) => {
+	return (dispatch:ThunkDispatch<any,any,anyAction>) => {
+		let request = new Request("/register",{
+			method:"POST",
+			headers:{"Content-Type":"application/json"},
+			body:JSON.stringify(user)
+		})
+		handleLogin(request,"register",dispatch);
+	}
+}
 
+const handleLogin = async (request:Request,act:string,dispatch:ThunkDispatch<any,any,AnyAction>) => {
+	dispatch(loading());
+	const response = await fetch(request);
+	dispatch(stopLoading());
+	if(!response) {
+		dispatch(logoutFailed("Server never responded. Resetting");
+		return;
+	}
+	if(response.ok) {
+		switch(act) {
+			case "register":
+				dispatch(registerSuccess());
+				return;
+			default:
+				return;
+		}
+	} else {
+		let errorMessage = " Server responded with a status "+response.status+" "+response.statusText;
+		switch(act) {
+			case "register":
+				if(response.status === 409) {
+					dispatch(registerFailed("Username already in use"));
+					return;
+				}
+				dispatch(registerFailed("Register failed."+errorMessage));
+				return;
+			default:
+				return;
+		}
+	}
+}
 
 //ACTION CREATORS
 
